d6a22439ab3ab5aa93712e415a2ff9cfc0c8cba0
soukainna <soukaina1lafdili@gmail.com>
2022-08-29 12:56:55 +0200

    add sounds to my page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var lol = document.querySelectorAll("button")
 for (var i = 0; i < lol.length; i++)
 {
     lol[i].addEventListener("click", function(){
-        var audio = new
+        var audio = new Audio('sounds/tom-1.mp3')
+        audio.play();
     })
 }

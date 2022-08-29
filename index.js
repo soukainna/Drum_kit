2f45e45b83d30c78a2270e10cb52c2508770c63b
soukainna <soukaina1lafdili@gmail.com>
2022-08-29 14:37:21 +0200

    put many sounds

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
-var lol = document.querySelectorAll("button")
+var lol = document.querySelectorAll(".drum")
 
 for (var i = 0; i < lol.length; i++)
 {
     lol[i].addEventListener("click", function(){
-        var audio = new Audio('sounds/tom-1.mp3')
-        audio.play();
+       
+        var b = this.innerHTML
+        console.log(b)
+        switch (b){
+            case 'w':
+            {
+                var audio = new Audio('sounds/crash.mp3')
+                audio.play()
+                break;
+            }
+            case 'a':
+            {
+                var audio = new Audio('sounds/kick-bass.mp3')
+                audio.play()
+                break;
+            }
+            case 's':
+            {
+                var audio = new Audio('sounds/snare.mp3')
+                audio.play()
+                break;
+            }
+            case 'd':
+            {
+                var audio = new Audio('sounds/tom-1.mp3')
+                audio.play()
+                break;
+            }
+            case 'j':
+            {
+                var audio = new Audio('sounds/tom-2.mp3')
+                audio.play()
+                break;
+            }
+            case 'k':
+            {
+                var audio = new Audio('sounds/tom-3.mp3')
+                audio.play()
+                break;
+            }
+            case 'l':
+            {
+                var audio = new Audio('sounds/tom-4.mp3')
+                audio.play()
+                break;
+            }
+            default :
+                console.log('Error')
+
+        }
     })
 }
